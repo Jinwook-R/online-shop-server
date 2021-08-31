853983fae004307b2e2351693d75e0c910adf7f6
Jinwook-R <jwlee_dev@naver.com>
2021-08-31 14:14:44 +0900

    new commit

---
+++ src/common/guard.js
@@ -0,0 +1,9 @@
+module.exports = {
+  guard: (redirectPath) => (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect(redirectPath); // if not auth
+  },
+};
+   
